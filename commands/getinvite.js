89dc2f1d39cd77436477fe2c6bf6710b49465abe
Gus Caplan <gustavcaplan@gmail.com>
2016-08-09 21:37:52 +0000

    meh

--- commands/getinvite.js
+++ commands/getinvite.js
@@ -5,8 +5,11 @@ var request = require('request');
 
 module.exports = {
     main: function(bot, msg, settings) {
-        var args = msg.content;
-        var url = "https://g.1536.cf/i/"+encodeURIComponent(args)+"/"+settings.config.internalkey;
+        var args = msg.content.split(' ');
+        var method = args[0];
+        args.shift();
+        args = args.join(' ');
+        var url = `https://g.1536.cf/i/${method}/${encodeURIComponent(args)}/${settings.config.internalkey}`;
         request(url, function (error, response, body) {
           if (!error && response.statusCode == 200) {
               var code = JSON.parse(body).url;
