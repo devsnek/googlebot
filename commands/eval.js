5a663048bf7195e5798091024fc3b432e2ce0c61
Gus Caplan <gustavcaplan@gmail.com>
2016-08-22 17:28:15 +0000

    preparing for big update

--- commands/eval.js
+++ commands/eval.js
@@ -1,41 +1,21 @@
-const util = require('util');
-const vm = require('vm');
-
-const sandbox = {
-  bot: { internal: { token: 'how about no' } }
-};
-
-const context = new vm.createContext(sandbox);
-
 module.exports = {
      main: function(bot, msg, settings, bots) {
-        let args = msg.content;
         if (msg.author.id == settings.OWNERID) {
-          bot.log('EVAL WAS RUN!');
-          try {
-            var res = eval(args);
-            if (typeof res != 'string') {
-              res = util.inspect(res);
+            args = msg.content;
+            bot.log('EVAL WAS RUN!');
+            try {
+                var res = eval(args);
+                if (typeof res != 'string') {
+                    res = require("util").inspect(res);
+                }
             }
-          } catch (err) {
-            var res = err.message;
-          }
-          bot.sendMessage(msg, "```js\n"+res+"\n```");
-        } else {
-          try {
-            let script = new vm.Script(args);
-            var res = script.runInContext(context);
-            if (typeof res != 'string') {
-              res = util.inspect(res);
+            catch (err) {
+                var res = err.message;
             }
-          } catch (err) {
-            var res = err.message;
-          }
-          bot.sendMessage(msg, "```js\n"+res+"\n```");
+            bot.sendMessage(msg, "```js\n"+res+"\n```");
         }
      },
      args: '',
      help: 'run some eval, bruh',
      hide: true
 };
-
