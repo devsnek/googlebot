362f66e0beb65e1de8eb639fffe02c4ec3a15aa9
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-10-25 22:30:53 -0500

    make things good for matt

--- backend/routes/invite.js
+++ backend/routes/invite.js
@@ -4,7 +4,16 @@ const config = require('../../config.json');
 const axios = require('axios');
 
 router.get('/', (req, res) => {
-  res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${config.backend.CLIENT_ID}&permissions=${config.backend.PERMISSIONS}&redirect_uri=${encodeURIComponent(config.backend.INVITE_REDIR)}&scope=bot&response_type=code`);
+  const INVITE_REDIR = req.query.redirect_uri || config.backend.INVITE_REDIR;
+  res.redirect(
+`https://discordapp.com/oauth2/authorize?
+client_id=${config.backend.CLIENT_ID}
+&permissions=${config.backend.PERMISSIONS}
+&redirect_uri=${encodeURIComponent(INVITE_REDIR)}
+&scope=bot
+&response_type=code
+${req.query.state ? `&state=${req.query.state}` : ''}`.replace(/\n|%0A/g, '')
+  );
 });
 
 router.get('/callback', async (req, res) => {
