4ce39bd9ea461017ea649236ea32d187302ef667
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-10-29 17:20:38 -0500

    fix purge command

--- commands/purge.js
+++ commands/purge.js
@@ -3,7 +3,11 @@ module.exports = {
     let count = parseInt(message.content) || 5;
     let messages = await message.channel.fetchMessages({limit: 100})
     messages = messages.array().filter(m => m.author.id === message.client.user.id).slice(0, count + 1);
-    message.channel.bulkDelete(messages);
+    try {
+      await message.channel.bulkDelete(messages);
+    } catch (err) {
+      messages.map(m => m.delete());
+    }
   },
   help: 'deletes messages from channel, default 5',
   args: '<count>',
