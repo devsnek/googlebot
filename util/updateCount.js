cb5ac82aa6d9345feec929d176d1b76d4d0203ea
Gus Caplan <gustavcaplan@gmail.com>
2016-10-29 19:02:19 +0000

    add tags to gitignore

--- util/updateCount.js
+++ util/updateCount.js
@@ -10,12 +10,16 @@ const updateCarbon = count => {
   console.log('updating carbon');
   superagent.post('https://www.carbonitex.net/discord/data/botdata.php')
     .set({'Content-Type': 'application/json', 'cache-control': 'no-cache'})
-    .send({ servercount: count, key: config.stats.carbon })
+    .send({ servercount: count, key: config.stats.carbon }).end((err, res) => {
+      if (!err) console.log('SUCCESSFUL CARBON UPDATE', count);
+    });
 }
 
 const updateAbal = count => {
   console.log('updating abal');
   superagent.post('https://bots.discord.pw/api/bots/187406062989606912/stats')
     .set({ 'Content-Type': 'application/json', 'Authorization': config.stats.abal })
-    .send({ 'server_count': count })
+    .send({ 'server_count': count }).end((err, res) => {}).end((err, res) => {
+      if (!err) console.log('SUCCESSFUL ABAL UPDATE', count);
+    });
 }
