f1a22e120243c71c6db8fa5bdb0345682a4eba0b
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-10-30 10:02:40 -0500

    clean up

--- util/tulpize.js
+++ util/tulpize.js
@@ -1,14 +1,6 @@
 const START = /^\/\/|^["{\[~-] ?/g;
 const END = / ?["\]}~-]$/g;
 
-if (!Reflect.has(Array.prototype, 'last')) {
-  Object.defineProperty(Array.prototype, 'last', { // eslint-disable-line
-    get: function () {
-      return this[this.length - 1];
-    }
-  });
-}
-
 module.exports = (message) => {
   const x = [];
   for (let c of [message.content, message.cleanContent]) {
