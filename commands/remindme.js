c62b0885b90609d88cbb6770b72e95a9ebb4abc4
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-11-01 05:40:12 -0500

    make remindme great again

--- commands/remindme.js
+++ commands/remindme.js
@@ -5,9 +5,11 @@ module.exports = {
   main: async message => {
     let s = Sherlock.parse(message.content);
     let relative = s.startDate.getTime() - Date.now();
+    s.eventTitle = s.eventTitle.replace(/^to ?/, '');
     message.channel.sendMessage(`I will remind you to ${s.eventTitle} ${moment().add(relative, 'ms').fromNow()}`);
     setTimeout(() => {
-      message.channel.sendMessage(`${message.author} **REMINDER:** ${s.eventTitle}`);
+      let final = `**REMINDER:** ${s.eventTitle}`;
+      message.author.sendMessage(final).catch(() => message.channel.sendMessage(`${message.author} ${final}`));
     }, relative);
   },
   hide: true
