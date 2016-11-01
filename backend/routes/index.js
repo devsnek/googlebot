f4bc7c61f69e3363dd4f4c2a533052f5bfa83172
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-11-01 02:30:05 -0500

    fuk u googie

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const r = require('../../util/rethink');
 const superagent = require('superagent');
 const entities = new require('html-entities').XmlEntities; // eslint-disable-line
+const marked = require('marked');
 
 router.use(async (req, res, next) => {
   const quote = await superagent.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand');
@@ -16,6 +17,8 @@ router.get('/', (req, res) => {
 
 router.get('/help', async (req, res) => {
   res.locals.commands = await r.raw.db('google').table('commands').run();
+  let compliance = await superagent.get('https://raw.githubusercontent.com/Roadcrosser/Compliance/master/enduser.md');
+  res.locals.compliance = marked(compliance.text.replace('**- for end users**\n', ''));
   res.render('help');
 });
 
--- util/tulpize.js
+++ util/tulpize.js
@@ -1,7 +1,10 @@
+const SCARY = ['110373943822540800'];
+
 const START = /^\/\/|^["{\[~-] ?/g;
 const END = / ?["\]}~-]$/g;
 
 module.exports = (message) => {
+  if (SCARY.includes(message.guild.id)) return [message.content, message.cleanContent];
   const x = [];
   for (let c of [message.content, message.cleanContent]) {
     if (START.test(c)) {
