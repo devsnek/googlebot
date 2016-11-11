eb651a8d4fec32a68564f082da6112cf21d2451f
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-11-10 22:23:27 -0600

    fix fallback for kg

--- commands/knowledgegraph.js
+++ commands/knowledgegraph.js
@@ -1,5 +1,10 @@
 const superagent = require('superagent');
 
+const fallback = (client, msg, message) => {
+  msg.delete();
+  client.commands.search.main(message);
+}
+
 module.exports = {
   main: async message => {
     const client = message.client;
@@ -7,21 +12,16 @@ module.exports = {
     const msg = await message.channel.sendMessage('`Searching...`');
     client.log('KG: ', msg.guild.name, msg.guild.id, '|', args);
     const url = `https://kgsearch.googleapis.com/v1/entities:search?key=${client.config.google.kgKey}&limit=1&indent=True&query=${args}`;
-    superagent.get(url).end((err, res) => {
-      if (err) {
-        client.error(err.stack);
-        msg.delete();
-        return client.commands.search.main(message);
-      }
-      const kg = res.body.itemListElement[0].result;
-      const final = `**${kg.name} (${kg['@type'].filter(t => t !== 'Thing').map(t => t.replace(/([a-z])([A-Z])/g, '$1 $2')).join(', ')})**
+    const res = await superagent.get(url);
+    if (!res.body.itemListElement[0]) return fallback(client, msg, message);
+    const kg = res.body.itemListElement[0].result;
+    if (!kg.detailedDescription) return fallback(client, msg, message);
+    const final = `**${kg.name} (${kg['@type'].filter(t => t !== 'Thing').map(t => t.replace(/([a-z])([A-Z])/g, '$1 $2')).join(', ')})**
 ${kg.detailedDescription.articleBody}
 <${kg.detailedDescription.url}>`;
-      msg.edit(final).catch(err => {
-        client.error(err.stack);
-        msg.delete();
-        client.commands.search.main(message);
-      });
+    msg.edit(final).catch(err => {
+      client.error(err.stack);
+      fallback(client, msg, message);
     });
   },
   hide: true
