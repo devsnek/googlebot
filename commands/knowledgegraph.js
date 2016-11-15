734d079f9be6ae9187e35fbba35c46938fe4d23b
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-11-15 00:10:38 -0600

    make knowledgegraph types great again

--- commands/knowledgegraph.js
+++ commands/knowledgegraph.js
@@ -12,7 +12,9 @@ module.exports = {
     if (!res.body.itemListElement[0]) return client.commands.search.main(message, msg);
     const kg = res.body.itemListElement[0].result;
     if (!kg.detailedDescription) return client.commands.search.main(message, msg);
-    const title = `${kg.name} (${kg['@type'].filter(t => t !== 'Thing').map(t => t.replace(/([a-z])([A-Z])/g, '$1 $2')).join(', ')})`;
+    let types = kg['@type'].map(t => t.replace(/([a-z])([A-Z])/g, '$1 $2'));
+    if (types.length > 1) types = types.filter(t => t !== 'Thing')
+    const title = `${kg.name} ${types.length === 0 ? '' : '(' + types.join(', ') + ')'}`;
     const description = `${kg.detailedDescription.articleBody} [Learn More...](${kg.detailedDescription.url})`;
     msg.edit('', { embed: client.embed(kg.detailedDescription.url, title, description) }).catch(err => {
       client.error(err.stack);
