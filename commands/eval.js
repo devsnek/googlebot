12f2774c882d34d6f7b7482348f11d3a898583c2
Gus Caplan <me@gus.host>
2016-11-15 23:14:17 -0600

    clean up eval

--- commands/eval.js
+++ commands/eval.js
@@ -1,19 +1,16 @@
 module.exports = {
   main: async message => {
     const client = message.client;
-    if (client.config.OWNERS.includes(message.author.id)) {
-      var args = message.content;
-      client.log('EVAL WAS RUN!');
-      try {
-        var res = eval(args); // eslint-disable-line no-eval
-        if (typeof res !== 'string') {
-          res = require('util').inspect(res);
-        }
-      } catch (err) {
-        res = err.message;
-      }
-      message.channel.sendMessage('```js\n' + res + '\n```');
+    if (!client.config.OWNERS.includes(message.author.id)) return;
+    const args = message.content;
+    client.log('EVAL WAS RUN!');
+    try {
+      var res = eval(args); // eslint-disable-line no-eval
+      if (typeof res !== 'string') res = require('util').inspect(res);
+    } catch (err) {
+      res = err.message;
     }
+    message.channel.sendCode('js', res);
   },
   args: '',
   help: 'run some eval, bruh',
