4eb43d40e0022d17ff2fcd16882d1d3da64bfb61
Hackzzila <admin@hackzzila.com>
2016-11-17 07:17:47 -0600

    Fix bugs 👀

--- commands/wikipedia.js
+++ commands/wikipedia.js
@@ -11,5 +11,8 @@ module.exports = {
     const url = `https://wikipedia.org/wiki/${encodeURIComponent(page.title)}`;
     const embed = message.client.util.embed(url, page.title, page.extract.substring(0, 500) + `... [Read more](${url})`);
     msg.edit('', { embed });
-  }
+  },
+  args: '<query>',
+  help: 'Search Wikipedia',
+  catagory: 'general'
 }
