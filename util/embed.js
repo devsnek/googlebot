a26516add5c7419b142fecc482df79cc6a1cb238
Gus Caplan <me@gus.host>
2016-11-18 08:35:24 -0600

    make embeds better

--- util/embed.js
+++ util/embed.js
@@ -1,13 +1,14 @@
 module.exports = (url, title = url, description = 'ᅠ') => {
   if (description !== 'ᅠ') description += '\n';
-  console.log(url);
   return {
     title: title,
     description: `${description}
 [Help keep Googlebot running](https://www.change.org/p/google-inc-help-googlebot-not-die/)
 [Donate to keep Googlebot alive](https://patreon.com/guscaplan)\n\u200b`,
-    url: url,
+    url,
     timestamp: new Date(),
+    video: { url },
+    image: { url },
     footer: {
       text: 'Powered by Google',
       icon_url: 'https://google.com/favicon.ico'
