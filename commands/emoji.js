89e69a7d4d6e44a3073e42f1b2c207cf86eb87ff
Gus Caplan <me@gus.host>
2016-11-29 15:51:57 -0600

    fix emoji

--- commands/emoji.js
+++ commands/emoji.js
@@ -2,7 +2,7 @@ const superagent = require('superagent');
 
 module.exports = {
   main: async message => {
-    const res = await superagent.get('http://emoji.getdango.com/api/emoji?q=' + message.content)
+    const res = await superagent.get('https://emoji.getdango.com/api/emoji?q=' + message.content)
     let final = res.body.results.map(r => r.text).slice(0, 7).join(' ');
     message.channel.sendMessage(final);
   },
