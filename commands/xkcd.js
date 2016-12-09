7f334e9e4351313e63ec132b73ce4c48539e3825
Gus Caplan <me@gus.host>
2016-12-09 17:28:50 -0600

    support numbers for xkcd command

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -2,9 +2,14 @@ const superagent = require('superagent');
 
 module.exports = {
   main: async message => {
-    let res = await superagent.get(`https://relevantxkcd.appspot.com/process?action=xkcd&query=${message.content}`)
-    const comicnum = res.text.split(' ')[2].replace('\n', '');
-    res = await superagent.get(`https://xkcd.com/${comicnum}/info.0.json`)
+    let comicnum;
+    if (isNaN(parseInt(message.content))) {
+      let res = await superagent.get(`https://relevantxkcd.appspot.com/process?action=xkcd&query=${message.content}`)
+      comicnum = res.text.split(' ')[2].replace('\n', '');
+    } else {
+      comicnum = parseInt(message.content);
+    }
+    const res = await superagent.get(`https://xkcd.com/${comicnum}/info.0.json`)
     let comic = res.body;
     let final = `XKCD ${comic.num} **${comic.safe_title}**
 _*${comic.alt}*_
