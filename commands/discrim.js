ad889550394978efc19119bb6cb076caa478e7eb
Gus Caplan <me@gus.host>
2016-12-11 16:42:53 -0600

    how could i have been this stupid

--- commands/discrim.js
+++ commands/discrim.js
@@ -1,11 +1,10 @@
 module.exports = {
   main: async (message) => {
     const client = message.client;
-    let res = await client.shard.broadcastEval(`this.users.filter(x => x.discriminator === "${message.content}").map(u => u.id)`);
+    let res = await client.shard.broadcastEval(`this.users.filter(x => x.discriminator === "${message.content}").map(u => u.username)`);
     res = [].concat(...res);
     res = res.filter((e, i, a) => i === a.indexOf(e));
-    res = await Promise.all(res.map(u => client.fetchUser(u)));
-    message.channel.sendMessage(`**${res.length} users with discrim ${message.content}:**\n${res.map(u => u.username).join(', ')}`)
+    message.channel.sendMessage(`**${res.length} users with discrim ${message.content}:**\n${res.join(', ')}`);
   },
   hide: true
 }
