a8dbeef6186a6cd07adf302dbbbca1614421829f
Gus Caplan <me@gus.host>
2016-12-20 01:04:15 -0600

    rewrite stats

--- commands/stats.js
+++ commands/stats.js
@@ -1,17 +1,18 @@
 module.exports = {
   main: async message => {
     const client = message.client;
-    const { users, channels, guilds } = await client.util.fetchStats();
+    const { users, channels, guilds, uptimes } = await client.util.fetchStats();
     const memUsage = (await client.shard.broadcastEval('process.memoryUsage().heapUsed / 1024 / 1024')).reduce((a, b) => a + b);
-    let final = `STATISTICS
-• Mem Usage    : ${memUsage.toFixed(2)} MB
-• Uptime       : ${client.util.toHHMMSS(client.uptime / 1000)}
-• Users        : ${users.toLocaleString()}
-• Channels     : ${channels.toLocaleString()}
-• Guilds       : ${guilds.toLocaleString()}
-• Discord.js   : v${require('../node_modules/discord.js/package.json').version}
-• Shard        : ${Number(client.shard.id) + 1}/${client.shard.count}`;
-    message.channel.sendCode('xl', final);
+    let final = `**STATISTICS**
+**• Mem Usage:** ${memUsage.toFixed(2)} MB
+**• Uptime:** ${client.util.toHHMMSS(client.uptime / 1000)}
+**• Users:** ${users.toLocaleString()}
+**• Channels:** ${channels.toLocaleString()}
+**• Guilds:** ${guilds.toLocaleString()}
+**• Library:** Discord.js v${require('../node_modules/discord.js/package.json').version}
+**• Shard:** ${Number(client.shard.id) + 1}/${client.shard.count}
+**• Uptimes:** ${uptimes}`;
+    message.channel.sendMessage(final);
   },
   hide: true
 }
--- util/fetchStats.js
+++ util/fetchStats.js
@@ -3,6 +3,7 @@ module.exports = (client) => {
     const users = (await client.shard.broadcastEval('this.guilds.map(g => g.memberCount).reduce((a, b) => a + b)')).reduce((a, b) => a + b);
     const channels = (await client.shard.broadcastEval('this.channels.size')).reduce((a, b) => a + b);
     const guilds = (await client.shard.broadcastEval('this.guilds.size')).reduce((a, b) => a + b);
-    return { users, channels, guilds };
+    const uptimes = (await client.shard.broadcastEval('this.uptime')).map(x => client.util.toHHMMSS(x / 1000));
+    return { users, channels, guilds, uptimes };
   }
 }
