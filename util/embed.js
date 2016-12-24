721cfbe243d41a53edb355e1fc48fb30f061f063
Gus Caplan <me@gus.host>
2016-12-24 02:19:52 -0600

    tidy up embeds

--- util/embed.js
+++ util/embed.js
@@ -1,8 +1,8 @@
 module.exports = (url, title = url, description = 'ᅠ') => {
   if (description !== 'ᅠ') description += '\n';
   return {
-    title: title,
-    description: `${description}\n[Donate to keep Googlebot alive](https://patreon.com/guscaplan)\n\u200b`,
+    title,
+    description: `${description}\n[Donate to keep Googlebot alive](https://patreon.com/guscaplan)`,
     url,
     timestamp: new Date(),
     video: { url },
