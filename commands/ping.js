d33bf88d56c404b375288261adbce69ef889e584
Gus Caplan <me@gus.host>
2017-01-03 00:48:23 -0600

    make ping command better

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,8 @@
 module.exports = {
-  main: async message => message.channel.send(`pong \`${Math.round(message.client.ping)}ms\``),
+  main: async message => {
+    const msg = await message.channel.send('Pong!');
+    msg.edit(`Pong! WS Ping: \`${Math.round(message.client.ping)}ms\` | HTTP Ping: \`${msg.createdTimestamp - message.createdTimestamp}ms\``);
+  },
   help: 'ping pong ping pong',
   args: '',
   catagory: 'util'
