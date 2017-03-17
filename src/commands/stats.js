fa17e7baaed487c684697422d0aaa93c22a4c29f
Gus Caplan <me@gus.host>
2017-03-17 17:21:00 -0500

    amazing stats

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -3,6 +3,8 @@ const stripIndent = require('common-tags').stripIndent;
 module.exports = {
   main: (message) => {
     const client = message.client;
+    const eventCounter = client.eventCounter;
+    const presencePercent = (((eventCounter.events.PRESENCE_UPDATE / eventCounter.total) * 100) || 0.00).toFixed(2);
     message.channel.send(stripIndent`
       **STATISTICS**
       **• Mem Usage:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
@@ -12,6 +14,8 @@ module.exports = {
       **• Guilds:** ${client.guilds.size.toLocaleString()}
       **• Library:** Discord.js v${require('discord.js').version}
       **• Shard:** ${Number(message.guild.shardID) + 1}/${client.ws.shardCount}
+
+      ${presencePercent}% of ${eventCounter.total.toLocaleString()} total events are presence updates.
     `);
   },
   hide: true,
