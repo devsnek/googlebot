c48edfe7d14ccce21ee094030278197fd2ad8620
Gus Caplan <me@gus.host>
2017-07-24 12:48:51 -0500

    rewrite for better

--- src/discord/Client.js
+++ src/discord/Client.js
@@ -1,6 +1,8 @@
 const EventEmitter = require('events');
 const WebSocketConnection = require('./WebSocketConnection');
 const Router = require('./rest/Router');
+const EventCounter = require('../util/EventCounter');
+const logger = require('../util/Logger');
 
 class Client extends EventEmitter {
   constructor(options = {}) {
@@ -10,6 +12,7 @@ class Client extends EventEmitter {
     this.token = null;
 
     this.shard_queue = [];
+    this.shard_count = null;
 
     this.rest = new Router(this);
 
@@ -19,6 +22,8 @@ class Client extends EventEmitter {
     this.channels = {
       get size() { return Object.keys(this).length; },
     };
+
+    this.eventCounter = new EventCounter();
   }
 
   get api() {
@@ -30,7 +35,7 @@ class Client extends EventEmitter {
     return this.api.gateway.bot.get().then((res) => {
       this.gateway = res.url;
       this.shard_queue.push(...Array.from({ length: res.shards }, (_, i) => i));
-      this.shard_queue.shard_count = res.shards;
+      this.shard_count = res.shards;
       this.spawn();
     });
   }
@@ -38,20 +43,22 @@ class Client extends EventEmitter {
   spawn(id) {
     if (id) {
       this.shard_queue.push(id);
-      if (this.hard_queue.length === 1) return;
+      if (this.shard_queue.length > 1) return;
     }
     const shard_id = this.shard_queue.shift();
     if (shard_id == null) return; // eslint-disable-line eqeqeq
+    logger.log('SPAWNING', shard_id);
     if (typeof shard_id === 'function') {
       shard_id();
     } else {
       const shard = new WebSocketConnection(this, {
         shard_id,
-        shard_count: this.shard_queue.shard_count,
+        shard_count: this.shard_count,
       });
       shard.on('packet', (packet) => {
+        this.eventCounter.trigger(packet.t ? packet.t : `OP_${packet.op}`);
         if (packet.t) this.emit(packet.t, packet.d, packet.shard_id);
-        if (packet.t === 'READY') setTimeout(this.spawn.bind(this), 5e3);
+        if (packet.t === 'READY') setTimeout(() => this.spawn(), 5e3);
       });
       shard.connect(this.gateway);
     }
---
+++ src/util/EventCounter.js
@@ -0,0 +1,38 @@
+class EventCounter {
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.events = {};
+    this.frequency = 0;
+    this.total = 0;
+    this.start = null;
+  }
+
+  trigger(event) {
+    if (!this.events[event]) this.events[event] = 0;
+    this.events[event]++;
+
+    if (!this.start) this.start = new Date();
+    this.frequency = ++this.total / (new Date() - this.start) * 1000;
+  }
+
+  inspect() {
+    if (Object.keys(this.events).length <= 1) return `EventCounter ${this.events}`;
+    const entries = Object.entries(this.events);
+    const ops = entries.filter(e => e[0].startsWith('OP')).sort((a, b) => b[1] - a[1]);
+    const events = entries.filter(e => !e[0].startsWith('OP')).sort((a, b) => b[1] - a[1]);
+    return `EventCounter {
+  Frequency => ${this.frequency.toLocaleString()} events/sec,
+  Total => ${this.total.toLocaleString()} events,
+  Start => ${this.start.toUTCString()},
+  //  OPs  //
+  ${ops.map(([k, v]) => `${k} => ${v.toLocaleString()}`).join(',\n  ')}
+  // Events //
+  ${events.map(([k, v]) => `${k} => ${v.toLocaleString()}`).join(',\n  ')}
+}`;
+  }
+}
+
+module.exports = EventCounter;
