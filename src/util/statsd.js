cd163563540f1bc284a4bcd8a9061784f155fa12
Gus Caplan <me@gus.host>
2017-08-05 19:44:24 -0500

    change scope of error handler

--- src/util/statsd.js
+++ src/util/statsd.js
@@ -3,10 +3,10 @@ const StatsD = require('hot-shots');
 const logger = require('./Logger');
 const config = require('../../config.json');
 
-const client = new StatsD(config.statsd);
-
-client.socket.on('error', (err) => {
-  logger.error('STATSD ERROR ', err.stack);
-});
+const client = new StatsD(Object.assign({
+  errorHandler(error) {
+    logger.error('STATSD ERROR', error.stack);
+  },
+}, config.statsd));
 
 module.exports = client;
