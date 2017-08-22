28f35fe6b1cf58c785d0e5f179d577af66ef2c6f
Gus Caplan <me@gus.host>
2017-08-21 21:57:59 -0700

    re-enable stats

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ require('./util/blocked')((n) => logger.error('UV BLOCK', n));
 
 client.raven = require('./util/raven');
 client.ua = require('./util/ua');
+client.stats = require('./util/stats');
 
 let prefix;
 const commands = client.commands = require('./commands');
@@ -47,7 +48,7 @@ client.on('MESSAGE_CREATE', (message, shard_id) => {
     raven_context.command = { command: command.name, args };
     logger.log('COMMAND', shard_id, command.name, `nsfw=${message.channel.nsfw}`);
     command(message);
-    // client.stats.increment(`commands.${command.name}`);
+    client.stats.increment(`commands.${command.name}`);
   } catch (err) {
     const event = client.raven.captureException(err, {
       extra: raven_context,
@@ -58,23 +59,23 @@ client.on('MESSAGE_CREATE', (message, shard_id) => {
 
 client.on('CONNECTING', () => {
   logger.log('SPAWNING COUNT', client.shard_count);
-  // client.stats.gauge('shard_count', client.shard_count);
+  client.stats.gauge('shard_count', client.shard_count);
   setTimeout(() => {
     updateStats();
     setInterval(() => updateStats(), 60e3);
   }, client.shard_count * 6e3);
 });
 
-// client.on('SHARD_STATUS', (id, status) => {
-//   client.stats.gauge(`shards.${id}`, status);
-// });
+client.on('SHARD_STATUS', (id, status) => {
+  client.stats.gauge(`shards.${id}`, status);
+});
 
 function updateStats() {
   if (client.unavailable > 0.07) return;
   logger.log('GUILD COUNT', client.guilds.size);
   logger.log('CHANNEL COUNT', client.channels.size);
-  // client.stats.gauge('guilds', client.guilds.size);
-  // client.stats.gauge('channels', client.channels.size);
+  client.stats.gauge('guilds', client.guilds.size);
+  client.stats.gauge('channels', client.channels.size);
 }
 
 client.login(config.tokens[process.env.NODE_ENV]);
